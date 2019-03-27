5d810af01d098d24f9b2b68da8f9775b21a59268
shaofengfeng <shaofengfeng@xiaomi.com>
2019-03-27 18:27:59 +0800

    normal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ApplicationContext ioc=new AnnotationConfigApplicationContext(AppConfig.class);
         BeanOne beanOne=(BeanOne) ioc.getBean("beanOne");
         beanOne.doSomething();
+//        ((AnnotationConfigApplicationContext) ioc).close();
+        System.out.println(beanOne.getMessage());
 
-        ((AnnotationConfigApplicationContext) ioc).close();
     }
 }
--- src/main/java/bean/BeanOne.java
+++ src/main/java/bean/BeanOne.java
@@ -1,16 +1,27 @@
 package bean;
 
+import jdk.nashorn.internal.objects.annotations.Property;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 @Component
 public class BeanOne {
-    @Resource
+
+    @Autowired
     private BeanTwo beanTwo;
+
+    @Value("m")
+    private String  message;
 
     public void doSomething(){
         System.out.println("Inside do doSomething() method of BeanOne");
         beanTwo.doSomething();
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
